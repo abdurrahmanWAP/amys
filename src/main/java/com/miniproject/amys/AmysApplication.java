351ec250d98f5eb8f79d8f0cdb55a7020fd59e79
abdurrahmanWAP <abdurrahmanwap@gmail.com>
2026-02-17 21:24:56 +0700

    Inisiate Set Up Project

---
+++ src/main/java/com/miniproject/amys/AmysApplication.java
@@ -0,0 +1,13 @@
+package com.miniproject.amys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmysApplication.class, args);
+	}
+
+}
